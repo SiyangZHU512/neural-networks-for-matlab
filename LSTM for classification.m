c9f3819afde661c167cce27223506abb7731eea6
SiyangZHU512 <140038042+SiyangZHU512@users.noreply.github.com>
2023-08-05 14:39:16 +0800

    Add files via upload

---
+++ LSTM for classification.m
@@ -0,0 +1,81 @@
+clc
+clear all
+close all
+clc
+warning off
+%%
+load fisheriris
+
+species=grp2idx(species)
+meas=mapminmax(meas)
+temp=randperm(size(species,1))
+P_train=meas(temp(1:80),:)'
+I_train=species(temp(1:80),:)'
+P_test=meas(temp(81:end),:)'
+I_test=species(temp(81:end),:)'
+M=size(P_train,2)
+N=size(P_test,2)
+%%
+[P_train,ps_input]=mapminmax(P_train,0,1)
+P_test=mapminmax("apply",P_test,ps_input)
+T_test=categorical(I_test)'
+T_train=categorical(I_train)'
+P_train =double(reshape(P_train,4,1,1,M))
+P_test=double(reshape(P_test,4,1,1,N))
+%%
+for i=1:M
+    p_train{i,1}=P_train(:,:,1,i)
+end
+for i=1:N
+    p_test{i,1}=P_test(:,:,1,i)
+end
+%%
+layers=[
+    sequenceInputLayer(4)
+    lstmLayer(6,'OutputMode','last')
+    reluLayer
+
+    fullyConnectedLayer(3)
+    softmaxLayer
+    classificationLayer];
+%%
+options=trainingOptions('adam', ...
+   'minibatchsize',100,...
+   'MaxEpochs',2400, ...       % training for 800 times
+    'InitialLearnRate',1e-3, ...
+    'L2Regularization',1e-04,...
+    'LearnRateSchedule','piecewise', ...
+    'LearnRateDropFactor',0.1, ...
+    'LearnRateDropPeriod',500, ...
+    'Shuffle','every-epoch', ...
+    'ValidationPatience', Inf,...
+    'Plots','training-progress', ...
+    'Verbose',false)
+%%
+net=trainNetwork(p_train,T_train,layers,options)
+T_sim1=predict(net,p_train)
+T_sim2=predict(net,p_test)
+%%
+T_sim1=vec2ind(T_sim1')
+T_sim2=vec2ind(T_sim2')
+%%
+error1=sum((T_sim1==I_train))/M*100
+error2=sum((T_sim2==I_test))/N*100
+%%
+figure(1)
+plot(1:N,I_test,'r-*')
+hold on
+plot(1:N,T_sim2,'b:o')
+grid on
+legend('real class','predicted class')
+xlabel('order')
+ylabel('classes')
+string = {'LSTM netwrok';
+          ['estimation accuracy= ' num2str(error2) '%']};
+title(string)
+figure
+cm=confusionchart(I_test,T_sim2)
+cm.Title='confusion matrix for test data'
+cm.ColumnSummary='column-normalized'
+cm.RowSummary='row-normalized'
+
---
+++ LSTM for time series.m
@@ -0,0 +1,91 @@
+warning off
+close all
+clear 
+clc
+%%
+A=xlsread('D:\桌面\logit.xlsx',2,'A1:A404')
+%%
+num_sam=length(A)
+kim=10   
+zim=1
+figure(1)
+plot(1:num_sam,A)
+%%
+for i=1:num_sam-kim-zim+1
+   A1(i,:)=[reshape(A(i:i+kim-1),1,kim),A(i+kim+zim-1)]
+end
+%%
+temp=randperm(size(A1,1))
+P_train=A1(temp(1:300),1:10)'
+I_train=A1(temp(1:300),11)'
+P_test=A1(temp(301:end),1:10)'
+I_test=A1(temp(301:end),11)'
+M=size(P_train,2)
+N=size(P_test,2)
+%%
+[P_train,ps_input]=mapminmax(P_train,0,1)
+P_test=mapminmax('apply',P_test,ps_input)
+[t_train,ps_output]=mapminmax(I_train,0,1)
+t_test=mapminmax("apply",I_test,ps_output)
+%%
+P_train=double(reshape(P_train,10,1,1,M))
+P_test=double(reshape(P_test,10,1,1,N))
+t_train=double(t_train)'
+t_test=double(t_test)'
+%%
+%%
+for i=1:M
+    p_train{i,1}=P_train(:,:,1,i)
+end
+for i=1:N
+    p_test{i,1}=P_test(:,:,1,i)
+end
+%%
+%%
+layers=[
+    sequenceInputLayer(10)
+    lstmLayer(4,'OutputMode','last')
+    reluLayer
+
+    fullyConnectedLayer(1)
+    regressionLayer];
+%%
+options=trainingOptions('adam', ...
+    'MiniBatchSize',30, ...    %training sample size for each time
+    'MaxEpochs',1200, ...       % training for 800 times
+    'InitialLearnRate',1e-3, ...
+    'LearnRateSchedule','piecewise', ...
+    'LearnRateDropFactor',0.5, ...
+    'LearnRateDropPeriod',800, ...
+    'Shuffle','every-epoch', ...
+    'Plots','training-progress', ...
+    'Verbose',false)
+%%
+net=trainNetwork(p_train,t_train,layers,options)
+T_sim1=predict(net,p_train)
+T_sim2=predict(net,p_test)
+%%
+T_sim1=mapminmax('reverse',T_sim1,ps_output)'
+T_sim2=mapminmax('reverse',T_sim2,ps_output)'
+%%
+MSE1=sqrt(sum((I_train-T_sim1).^2)/N)
+MSE2=sqrt(sum((I_test-T_sim2).^2)/N)
+%%
+figure(1)
+plot(1:N,I_test,'b-',1:N,T_sim2,'r-')
+legend('original data','predict value')
+title('LSTM :process of prediction')
+xlabel('order')
+ylabel('value')
+string = {'prediction VS real value';
+          ['estimation value error= ' num2str(MSE2)]};
+title(string)
+figure(2)
+plot(1:size(I_train,2),I_train,'b-',1:size(I_train,2),T_sim1,'r-')
+legend('original data','fitted value')
+title('LSTM:process of fit')
+xlabel('order')
+ylabel('value')
+string = {'fitted value VS real value';
+          ['estimation value error= ' num2str(MSE1)]};
+title(string)
